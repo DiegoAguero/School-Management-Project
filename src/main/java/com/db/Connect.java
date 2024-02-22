4b8c20663956978849940800aa1c8c13f651107f
Diego Tomas Aguero <diegotomasaguero4123@gmail.com>
2024-02-23 00:09:45 +0100

    Añado funcionalidad insertCareer

--- src/main/java/com/db/Connect.java
+++ src/main/java/com/db/Connect.java
@@ -11,7 +11,7 @@ public class Connect {
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/bd1", "root", "123456");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/escuela", "root", "123456");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -31,12 +31,21 @@ public class Connect {
     //Añadir objeto carrera
     public void insertCareer(Career career) throws SQLException{
         Connection connection = getConnection();
-        String SQLQuery = "INSERT INTO carreras (nombre) VALUES ?";
+        String SQLQuery = "INSERT INTO carreras (nombre) VALUES (?)";
         PreparedStatement st = connection.prepareStatement(SQLQuery);
-        //Esperar objeto carrera
+        
         st.setString(1, career.getName());
+        int rowsInserted = st.executeUpdate();
+        System.out.println("Rows inserted: " + rowsInserted);
         connection.close();
     }
+//    public void updateCareer() throws SQLException{
+//        Connection connection = getConnection();
+//        String SQLQuery = "UPDATE carreras SET nombre";
+//        PreparedStatement st = connection.prepareStatement(SQLQuery);
+//        st.setString("Waiting for Career's.OBJ", SQLQuery);
+//        connection.close();
+//    }
 //    public void deleteCareer() throws SQLException{
 //        Connection connection = getConnection();
 //        String SQLQuery = "DELETE FROM carreras (nombre) VALUES ?";
@@ -45,15 +54,7 @@ public class Connect {
 //        connection.close();
 //
 //    }
-//    public void updateCareer() throws SQLException{
-//        Connection connection = getConnection();
-//        String SQLQuery = "UPDATE carreras SET nombre";
-//        PreparedStatement st = connection.prepareStatement(SQLQuery);
-//        st.setString("Waiting for Career's.OBJ", SQLQuery);
-//        connection.close();
-//
-//
-//    }
+
 //    //Esto deberia devolver algo
 //    public void readCareer() throws SQLException{
 //        Connection connection = getConnection();
