b60fecd3d0226c5103b0566fd6f4a94c6fe0937b
Diego <diegotomasaguero4123@gmail.com>
2024-02-27 13:01:07 +0100

    Añado ver estudiantes por carrera

--- src/main/java/com/db/CareerQuery.java
+++ src/main/java/com/db/CareerQuery.java
@@ -35,4 +35,25 @@ public class CareerQuery{
             System.out.println(nameOfCareer + "\n");
         }
     }
+    public void seeStudentsFromCareer(Career career, Connection stablishConnection) throws SQLException{
+        String SQLQuery = "SELECT a.nombre, a.DNI, c.nombre AS carrera FROM alumnos a " + 
+                          "JOIN alumnos_materias am ON a.id = am.idAlumnos " + 
+                          "JOIN materias m ON am.idMaterias = m.id " + 
+                          "JOIN carreras c ON m.idCarreras = c.id " + 
+                          "WHERE c.nombre = '"+ career.getName() + "' " + 
+                          "GROUP BY a.nombre, a.DNI " +
+                          "ORDER BY a.nombre DESC";
+        Statement st = stablishConnection.createStatement();
+        ResultSet rs = st.executeQuery(SQLQuery);
+        while(rs.next()){
+            String nameOfStudent = rs.getString("nombre");
+            String DNIOfStudent = rs.getString("DNI");
+            String nameOfCareer = rs.getString("carrera");
+            System.out.println("Carrera: " + nameOfCareer); 
+            System.out.println("Nombre del estudiante: " + nameOfStudent);
+            System.out.println("DNI del estudiante: " + DNIOfStudent);
+            System.out.println("======================");
+
+        }
+    }
 }
--- src/main/java/com/school/Main.java
+++ src/main/java/com/school/Main.java
@@ -10,65 +10,102 @@ public class Main {
     // private static ArrayList<String> elementos = new ArrayList<>();
     public static void main(String[] args) throws SQLException{
         Scanner scanner = new Scanner(System.in);
-        boolean salir = false;
+        boolean out = false;
+        boolean out2 = false;
         Connection connection = Connect.getConnection();
         CareerQuery careerQuery = new CareerQuery();
-        while (!salir) {
-            System.out.println("1. Añadir carrera");
-            System.out.println("2. Actualizar carrera");
-            System.out.println("3. Ver carrera");
-            System.out.println("4. Borrar carrera");
-            System.out.println("5. Salir");
-            System.out.print("Seleccione una opción: ");
-            
-            int opcion = scanner.nextInt();
+        while (!out) {
+            System.out.println("1. Carreras");
+            System.out.println("2. Alumnos");
+            System.out.println("3. Matricular");
+            System.out.println("4. Salir");
+            System.out.println("Seleccione una opción: ");
+            byte option = scanner.nextByte();
             scanner.nextLine(); // Limpiar el buffer
-            
-            switch (opcion) {
-                case 1:
-                try {
-                        String name = scanner.nextLine();
-                        careerQuery.insertCareer(new Career(name), connection);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
 
+            switch (option) {
+                case 1:
+                    while(!out2){
+                        System.out.println("1. Añadir carrera");
+                        System.out.println("2. Actualizar carrera");
+                        System.out.println("3. Ver carrera");
+                        System.out.println("4. Borrar carrera");
+                        System.out.println("5. Ver alumnos de una carrera");
+                        System.out.println("6. Salir");
+                        System.out.print("Seleccione una opción: ");
+                        int careerOption = scanner.nextInt();
+                        scanner.nextLine(); // Limpiar el buffer
+                        switch (careerOption) {
+                            case 1:
+                            try {
+                                    String name = scanner.nextLine();
+                                    careerQuery.insertCareer(new Career(name), connection);
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case 2:
+                                try {
+                                    System.out.println("Insertar nombre antiguo de la carrera: ");
+                                    String oldName = scanner.nextLine();
+                                    System.out.println("Ingrese el nombre nuevo de la carrera: ");
+                                    String name = scanner.nextLine();
+                                    careerQuery.updateCareer(oldName, new Career(name), connection);
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case 3:
+                                try {
+                                    careerQuery.readCareer(connection);
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            case 4:
+                                try {
+                                    System.out.println("Ingrese el nombre de la carrera que desea eliminar: ");
+                                    String name = scanner.nextLine();
+                                    careerQuery.deleteCareer(new Career(name), connection);
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case 5:
+                                try {
+                                    System.out.println("Ingrese el nombre de la carrera que desea ver los alumnos: ");
+                                    String career = scanner.nextLine();
+                                    careerQuery.seeStudentsFromCareer(new Career(career), connection);
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            case 6:
+                                connection.close();
+                                out2 = true;
+                                break;
+                            default:
+                                System.out.println("Opción no válida");
+                        }
+                        break;
+                    }
+                    break;
                 case 2:
-                    try {
-                        System.out.println("Insertar nombre antiguo de la carrera: ");
-                        String oldName = scanner.nextLine();
-                        System.out.println("Ingrese el nombre nuevo de la carrera: ");
-                        String name = scanner.nextLine();
-                        careerQuery.updateCareer(oldName, new Career(name), connection);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
+                    //Student
                     break;
-                case 3:
-                    try {
-                        careerQuery.readCareer(connection);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
+                case 3: 
+                    //Enrol student 
+                    break;
                 case 4:
-                    try {
-                        System.out.println("Ingrese el nombre de la carrera que desea eliminar: ");
-                        String name = scanner.nextLine();
-                        careerQuery.deleteCareer(new Career(name), connection);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 5:
                     connection.close();
-                    salir = true;
+                    out = true;
                     break;
                 default:
                     System.out.println("Opción no válida");
             }
+
+            
+
         }
-        
+        scanner.close();
         System.out.println("¡Hasta luego!");
     }
      
